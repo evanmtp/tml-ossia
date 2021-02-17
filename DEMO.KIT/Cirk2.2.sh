fc15de084bba673c62fa0fb730701c51bd19765d
evanmtp <evan.montpellier@gmail.com>
2021-02-17 16:39:41 -0500

    Update Cirk 2.2 sensor presets and startup script

--- DEMO.KIT/Cirk2.2.sh
+++ DEMO.KIT/Cirk2.2.sh
@@ -4,4 +4,4 @@ open -a Max /Users/tml/src/tml-ossia/DEMO.KIT/circ.demo.sensors.maxpat
 sleep 20
 open -a Max2 /Users/tml/src/tml-ossia/DEMO.KIT/circ.demo.sonify.nav.maxpat
 sleep 20
-open -a Ableton\ Live\ 10\ Suite /Users/tml/src/PVH-cirk/PVH-demo-kit/\#wheeljuggle_7\ Project/#wheeljuggle_7.als
+open -a Ableton\ Live\ 10\ Suite /Users/tml/src/PVH-cirk/PVH-demo-kit/\#wheeljuggle_8\ Project/#wheeljuggle_8.als
