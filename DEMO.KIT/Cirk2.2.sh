21db350becb79500e4599b6bc8130b0e8783755f
Evan Montpellier <evan.montpellier@gmail.com>
2021-02-17 14:31:44 -0500

    Cirk2.2.sh - script to launch demo kit for Naël

---
+++ DEMO.KIT/Cirk2.2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+open -a Max /Users/syrinx/Github/tml-ossia/DEMO.KIT/circ.demo.sensors.maxpat
+sleep 10
+open -a Max\ 2 /Users/syrinx/Github/tml-ossia/DEMO.KIT/circ.demo.sonify.nav.maxpat
+sleep 10
+open -a Ableton\ Live\ 10\ Suite /Users/syrinx/Music/Ableton/Live\ Recordings/2021-01-27_evan_cohen\ Project/2021-01-27_evan_cohen.als
